99a7173baca0edcc8e40d97697f52d90251838df
jonat <jonat@WXI1144064>
2026-09-09 23:34:01 +0300

    9/9

---
+++ Views/FirstPage.xaml.cs
@@ -0,0 +1,15 @@
+namespace Candles.Views;
+
+public partial class FirstPage : ContentPage
+{
+    public FirstPage()
+	{
+		InitializeComponent();
+		Label label = new Label
+		{
+			Text = "yeello",
+			TextColor = Colors.DarkSlateBlue
+		};
+    }
+
+}
---
+++ Views/SecondPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Candles.Views;
+
+public partial class SecondPage : ContentPage
+{
+	public SecondPage()
+	{
+		InitializeComponent();
+	}
+}
